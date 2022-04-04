be6d6217068fecd094cb1e781261b3a22bf42678
rajyadav282 <94789028+rajyadav282@users.noreply.github.com>
2022-04-04 09:44:13 -0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -8,9 +8,9 @@ app.get("/",function(request,response){
 });
 
 app.post("/",function(req,res){
-  var num1=req.body.num1;
-  var num2=req.body.num2;
-  var result=num1+num2;
+  var num1=Number(req.body.num1);
+  var num2=Number(req.body.num2);
+  var result=Number(num1+num2);
   res.send("The result of the calculator is " + result);
 });
 
